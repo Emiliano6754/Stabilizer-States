36e00a471e90a2e05b7b0a5c3a3837de70ddf778
Emiliano Pastrana <emilianopm93@gmail.com>
2025-06-17 11:19:59 -0600

    Reworked displaced graphs loops

--- displaced_Qfunc.cpp
+++ displaced_Qfunc.cpp
@@ -185,93 +185,81 @@ void max_lClifford_distances(const unsigned int &n_qubits, const unsigned int &q
 
 // Calculates the Rényi entropy after all possible displacements for the state given in Qfunc and outputs them in entropies. Both are assumed to already be of size 2^n_qubits x 2^n_qubits. For 15 qubits this requires >16 GB of ram. To circunvent this, the calculated values of entropy must be directly stored in memory
 void calc_full_displaced_entropy(const Eigen::MatrixXd &Qfunc, const unsigned int &n_qubits, const unsigned int &qubitstate_size, Eigen::MatrixXd &entropies) {
+    struct thread_variables {
+        Eigen::Tensor<double, 0> entropy;
+    };
     Eigen::Array<Eigen::IndexPair<int>, 3, 1> contraction_indices = {Eigen::IndexPair<int>(0,0), Eigen::IndexPair<int>(1,1), Eigen::IndexPair<int>(2,2)};
-    #pragma omp parallel
-    {
-        Eigen::Tensor<double, 3> sym_Qfunc(n_qubits+1,n_qubits+1,n_qubits+1);
-        Eigen::Tensor<double, 0> entropy; // Necessary to force evaluation of the tensor expressions
-        #pragma omp for
-        for (unsigned int mu = 0; mu < qubitstate_size; mu++) {
-            for (unsigned int nu = 0; nu < qubitstate_size; nu++) {
-                sym_Qfunc.setZero();
-                for (unsigned int alpha = 0; alpha < qubitstate_size; alpha++) {
-                    for (unsigned int beta = 0; beta < qubitstate_size; beta++) {
-                        sym_Qfunc(std::popcount(alpha ^ mu), std::popcount(beta ^ nu), std::popcount(alpha ^ beta ^ mu ^ nu)) += Qfunc(alpha, beta); // Should test if it is faster to make the sums in symQfunc or Qfunc
-                    }
-                }
-                entropy = sym_Qfunc.contract(sym_Qfunc, contraction_indices);
-                entropies(mu, nu) = entropy(0);
-            }
-        }
-    }
+
+    for_all_displaced_symQ(n_qubits, qubitstate_size, Qfunc,
+    [&]()->thread_variables {
+        thread_variables vars;
+        return vars;
+    },
+    [&](const Eigen::Tensor<double, 3> &sym_Qfunc, thread_variables &thread_variables, const unsigned int &mu, const unsigned int &nu) {
+        thread_variables.entropy = sym_Qfunc.contract(sym_Qfunc, contraction_indices);
+        entropies(mu, nu) = thread_variables.entropy(0);
+    },
+    [&](const thread_variables &thread_variables) {
+
+    });
 }
 
 // Calculates the Rényi entropy after all possible displacements for the state given in Qfunc and outputs them in entropies. Both are assumed to already be of size 2^n_qubits x 2^n_qubits. Also stores the displacements required for maximum and minimum entropy in max_displacement and min_displacement, respectively, as pairs. For 15 qubits this requires >16 GB of ram. To circunvent this, the calculated values of entropy must be directly stored in memory
 void calc_full_displaced_maxmin_entropy(const Eigen::MatrixXd &Qfunc, const unsigned int &n_qubits, const unsigned int &qubitstate_size, Eigen::MatrixXd &entropies, std::tuple<unsigned int, unsigned int> &max_displacement, std::tuple<unsigned int, unsigned int> &min_displacement) {
+    struct thread_variables {
+        Eigen::Tensor<double, 0> current_entropy;
+        double local_max_entropy = 0;
+        double local_min_entropy = 1;
+        std::tuple<unsigned int, unsigned int> local_max_displacement = {0, 0};
+        std::tuple<unsigned int, unsigned int> local_min_displacement = {0, 0};
+    };
     Eigen::Array<Eigen::IndexPair<int>, 3, 1> contraction_indices = {Eigen::IndexPair<int>(0,0), Eigen::IndexPair<int>(1,1), Eigen::IndexPair<int>(2,2)};
-    double global_max_entropy = 0;
-    double global_min_entropy = 1;
-    #pragma omp parallel
-    {
-        Eigen::Tensor<double, 3> sym_Qfunc(n_qubits+1,n_qubits+1,n_qubits+1);
-        Eigen::Tensor<double, 0> entropy; // Necessary to force evaluation of the tensor expressions
-
-        // Let each thread calculate a minimum/maximum and then start comparing their values as they finish
-        std::tuple<unsigned int, unsigned int> max_mu_nu = {0,0};
-        std::tuple<unsigned int, unsigned int> min_mu_nu = {0,0};
-        double max_entropy = 0;
-        double min_entropy = 1;
-        #pragma omp for nowait
-        for (unsigned int mu = 0; mu < qubitstate_size; mu++) {
-            for (unsigned int nu = 0; nu < qubitstate_size; nu++) {
-                sym_Qfunc.setZero();
-                for (unsigned int alpha = 0; alpha < qubitstate_size; alpha++) {
-                    for (unsigned int beta = 0; beta < qubitstate_size; beta++) {
-                        sym_Qfunc(std::popcount(alpha ^ mu), std::popcount(beta ^ nu), std::popcount(alpha ^ beta ^ mu ^ nu)) += Qfunc(alpha, beta);
-                    }
-                }
-                entropy = sym_Qfunc.contract(sym_Qfunc, contraction_indices);
-                entropies(mu, nu) = entropy(0);
-                if (entropy(0) > max_entropy) {
-                    max_entropy = entropy(0);
-                    max_mu_nu = {mu, nu};
-                }
-                if (entropy(0) < min_entropy) {
-                    min_entropy = entropy(0);
-                    min_mu_nu = {mu, nu};
-                }
-            }
+    double max_entropy = 0;
+    double min_entropy = 1;
+    
+    for_all_displaced_symQ(n_qubits, qubitstate_size, Qfunc,
+    [&]()->thread_variables {
+        thread_variables vars;
+        return vars;
+    },
+    [&](const Eigen::Tensor<double, 3> &sym_Qfunc, thread_variables &thread_variables, const unsigned int &mu, const unsigned int &nu) {
+        thread_variables.current_entropy = sym_Qfunc.contract(sym_Qfunc, contraction_indices);
+        entropies(mu, nu) = thread_variables.current_entropy(0);
+        if (thread_variables.current_entropy(0) > thread_variables.local_max_entropy) {
+            thread_variables.local_max_entropy = thread_variables.current_entropy(0);
+            thread_variables.local_max_displacement = {mu, nu};
         }
-        #pragma omp critical
-        {
-            if (max_entropy > global_max_entropy) {
-                global_max_entropy = max_entropy;
-                max_displacement = max_mu_nu;
-            }
-            if (min_entropy < global_min_entropy) {
-                global_min_entropy = min_entropy;
-                min_displacement = min_mu_nu;
-            }
+        if (thread_variables.current_entropy(0) < thread_variables.local_min_entropy) {
+            thread_variables.local_min_entropy = thread_variables.current_entropy(0);
+            thread_variables.local_min_displacement = {mu, nu};
         }
-    }
+    },
+    [&](const thread_variables &thread_variables) {
+        // Compare distances to thread local optimizers and maximize globally
+        if (thread_variables.local_max_entropy > max_entropy) {
+            max_entropy = thread_variables.local_max_entropy;
+            max_displacement = thread_variables.local_max_displacement;
+        }
+        if (thread_variables.local_min_entropy < min_entropy) {
+            min_entropy = thread_variables.local_min_entropy;
+            min_displacement = thread_variables.local_min_displacement;
+        }
+    });
 }
 
 void calc_all_displaced_symQ(const Eigen::MatrixXd &Qfunc, const unsigned int &n_qubits, const unsigned int &qubitstate_size, std::string filepath) {
-    #pragma omp parallel
-    {
-        Eigen::Tensor<double, 3> sym_Qfunc(n_qubits+1,n_qubits+1,n_qubits+1);
-        
-        #pragma omp for
-        for (unsigned int mu = 0; mu < qubitstate_size; mu++) {
-            for (unsigned int nu = 0; nu < qubitstate_size; nu++) {
-                sym_Qfunc.setZero();
-                for (unsigned int alpha = 0; alpha < qubitstate_size; alpha++) {
-                    for (unsigned int beta = 0; beta < qubitstate_size; beta++) {
-                        sym_Qfunc(std::popcount(alpha ^ mu), std::popcount(beta ^ nu), std::popcount(alpha ^ beta ^ mu ^ nu)) += Qfunc(alpha, beta); // Should test if it is faster to make the sums in symQfunc or Qfunc
-                    }
-                }
-                save_symQfunc(sym_Qfunc, filepath + "/" + std::to_string(mu) + "," + std::to_string(nu) + ".txt");
-            }
-        }
-    }
+    struct thread_variables {
+    };
+
+    for_all_displaced_symQ(n_qubits, qubitstate_size, Qfunc,
+    [&]()->thread_variables {
+        thread_variables vars;
+        return vars;
+    },
+    [&](const Eigen::Tensor<double, 3> &sym_Qfunc, const thread_variables &thread_variables, const unsigned int &mu, const unsigned int &nu) {
+       save_symQfunc(sym_Qfunc, filepath + "/" + std::to_string(mu) + "," + std::to_string(nu) + ".txt");
+    },
+    [&](const thread_variables &thread_variables) {
+
+    });
 }
